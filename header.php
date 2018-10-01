44242c9722a6374ea82934db63c119203105db05
susantha <susanthawarnapura@gmail.com>
2018-10-01 22:07:07 +0530

    initail commit

---
+++ header.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
+
+
+        <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400italic,700italic' rel='stylesheet' type='text/css'>
+
+
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+
+        <link rel="stylesheet" href="css/normalize.css" type="text/css">
+
+        <!-- custom css file -->
+        <link href="css/style.css" rel="stylesheet">
+
+        <title>Hello, world!</title>
+    </head>
+    <body>
---
+++ index.php
@@ -0,0 +1,203 @@
+<?php include 'header.php';?>
+
+    <section id="section-breaker" class="container main-content ani in">
+        <div class="row">
+            <div class="col-md-10 col-md-push-1">
+
+                <div class="clearfix text-justify">
+                    <h1 class="text-center">Main Heading</h1>
+                    <p>
+                        OK, so images can get quite complicated as we have a few variables to work with! For example the image below has had a caption entered in the WordPress image upload dialog box, this creates a [caption] shortcode which then in turn wraps the whole thing in a <code>div</code> with inline styling! Maybe one day they'll be able to use the <code>figure</code> and <code>figcaption</code> elements for all this. Additionally, images can be wrapped in links which, if you're using anything other than <code>color</code> or <code>text-decoration</code> to style your links can be problematic.
+                    </p>
+                    <div class="wp-caption alignnone">
+                        <a href="#"><img src="../images/blank.png" data-src="../images/sample-images/img_large.png" alt="Your Alt Tag" title="bmxisbest" class="size-large wp-image-28 img-responsive"></a>
+                        <p class="wp-caption-text">
+                            This is the optional caption.
+                        </p>
+                    </div>
+                    <p>
+                        The next issue we face is image alignment, users get the option of <em>None</em>, <em>Left</em>, <em>Right</em> &amp; <em>Center</em>. On top of this, they also get the options of <em>Thumbnail</em>, <em>Medium</em>, <em>Large</em> &amp; <em>Fullsize</em>. You'll probably want to add floats to style the image position so important to remember to clear these to stop images popping below the bottom of your articles.
+                    </p>
+                    <img src="../images/blank.png" data-src="../images/sample-images/img_medium.png" alt="Your Alt Title" title="Your Title" class="alignright img-responsive size-medium wp-image-28">
+                    <img src="../images/blank.png" data-src="../images/sample-images/img_thumb.png" alt="Your Alt Title" title="Your Title" class="alignleft size-thumbnail img-responsive wp-image-28">
+                    <img class="aligncenter size-medium img-responsive wp-image-28" title="Your Title" src="../images/blank.png" data-src="../images/sample-images/img_medium.png" alt="Your Alt Title">
+                    <p>
+                        Additionally, to add further confusion, images can be wrapped inside paragraph content, lets test some examples here.<img src="../images/sample-images/img_medium.png" alt="Your Alt Title" title="Your Title" class="alignright size-medium img-responsive wp-image-28">
+                        Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean lacinia bibendum nulla sed consectetur.<img src="../images/blank.png" data-src="../images/sample-images/img_thumb.png" alt="Your Alt Title" title="Your Title" class="alignleft size-thumbnail img-responsive wp-image-28">Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean lacinia bibendum nulla sed consectetur.<img src="../images/blank.png" data-src="../images/sample-images/img_thumb.png" alt="Your Alt Title" title="Your Title" class="aligncenter size-thumbnail img-responsive wp-image-28">Aenean lacinia bibendum nulla sed consectetur. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur.
+                    </p>
+                    <p>
+                        And then... Finally, users can insert a WordPress [gallery], which is kinda ugly and comes with some CSS stuck into the page to style it (which doesn't actually validate, nor does the markup for the gallery). The amount of columns in the gallery is also changable by the user, but the default is three so we'll work with that for our example with an added fouth image to test verticle spacing.
+                    </p>
+                    <ol>
+                        <li>Ordered list item one.</li>
+                        <li>Ordered list item two.</li>
+                        <li>Ordered list item three.</li>
+                        <li>Ordered list item four.</li>
+                        <li>By the way, Wordpress does not let you create nested lists through the visual editor.</li>
+                    </ol>
+                    <ul>
+                        <li>Unordered list item one.</li>
+                        <li>Unordered list item two.</li>
+                        <li>Unordered list item three.</li>
+                        <li>Unordered list item four.</li>
+                        <li>By the way, Wordpress does not let you create nested lists through the visual editor.</li>
+                    </ul>
+                    <blockquote>
+                        Currently WordPress blockquotes are just wrapped in blockquote tags and have no clear way for the user to define a source. Maybe one day they'll be more semantic (and easier to style) like the version below.
+                    </blockquote>
+                    <blockquote cite="http://html5doctor.com/blockquote-q-cite/">
+                        <p>
+                            HTML5 comes to our rescue with the footer element, allowing us to add semantically separate information about the quote.
+                        </p>
+                        <footer>
+                            <cite>
+                                <a href="http://html5doctor.com/blockquote-q-cite/">Oli Studholme, HTML5doctor.com</a>
+                            </cite>
+                        </footer>
+                    </blockquote>
+                    <h2 class="text-center">Level Two Heading</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                    <h3 class="text-center">Level Three Heading</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                    <h4 class="text-center">Level Four Heading</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                    <h5 class="text-center">Level Five Heading</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                    <h6 class="text-center">Level Six Heading</h6>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                    <p>
+                        This is a standard paragraph created using the WordPress TinyMCE text editor. It has a <strong>strong tag</strong>, an <em>em tag</em> and a <del>strikethrough</del> which is actually just the del element. There are a few more inline elements which are not in the WordPress admin but we should check for incase your users get busy with the copy and paste. These include <cite>citations</cite>, <abbr title="abbreviation">abbr</abbr>, bits of <code>code</code> and <var>variables</var>, <q>inline quotations</q>, <ins datetime="2011-12-08T20:19:53+00:00">inserted text</ins>, text that is <s>no longer accurate</s> or something <mark>so important</mark> you might want to mark it. We can also style subscript and superscript characters like C0<sub>2</sub>, here is our 2<sup>nd</sup> example. If they are feeling non-semantic they might even use <b>bold</b>, <i>italic</i>, or <small>small</small> elements too.&nbsp;Incidentally, these HTML4.01 tags have been given new life and semantic meaning in HTML5, you may be interested in reading this <a title="HTML5 Semantics" href="http://csswizardry.com/2011/01/html5-and-text-level-semantics">article by Harry Roberts</a> which gives a nice excuse to test a link.&nbsp;&nbsp;It is also worth noting in the "kitchen sink" view you can also add <span style="text-decoration: underline;">underline</span>&nbsp;styling and set <span style="color: #ff0000;">text color</span> with pesky inline CSS.
+                    </p>
+                    <p style="text-align: left;">
+                        Additionally, WordPress also sets text alignment with inline styles, like this left aligned paragraph.&nbsp;Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cras mattis consectetur purus sit amet fermentum.
+                    </p>
+                    <p style="text-align: right;">
+                        This is a right aligned paragraph.&nbsp;Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cras mattis consectetur purus sit amet fermentum.
+                    </p>
+                    <p style="text-align: justify;">
+                        This is a justified paragraph.&nbsp;Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cras mattis consectetur purus sit amet fermentum.
+                    </p>
+                    <p style="padding-left: 30px;">
+                        Finally, you also have the option of an indented paragraph.&nbsp;Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cras mattis consectetur purus sit amet fermentum.
+                    </p>
+                    <p>
+                        And last, and by no means least, users can also apply the <code>Address</code> tag to text like this:
+                    </p>
+                    <address>123 Example Street, Testville, West Madeupsburg, CSSland, 1234</address>
+                    <p>
+                        ...so there you have it, all our text elements
+                    </p>
+                    <div class="table-responsive">
+                        <table class="table table-bordered">
+                            <caption>Optional table caption.</caption>
+                            <thead>
+                            <tr>
+                                <th>
+                                    #
+                                </th>
+                                <th>
+                                    First Name
+                                </th>
+                                <th>
+                                    Last Name
+                                </th>
+                                <th>
+                                    Username
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <th scope="row">
+                                    1
+                                </th>
+                                <td>
+                                    Mark
+                                </td>
+                                <td>
+                                    Otto
+                                </td>
+                                <td>
+                                    @mdo
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    2
+                                </th>
+                                <td>
+                                    Jacob
+                                </td>
+                                <td>
+                                    Thornton
+                                </td>
+                                <td>
+                                    @fat
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    3
+                                </th>
+                                <td>
+                                    Larry
+                                </td>
+                                <td>
+                                    the Bird
+                                </td>
+                                <td>
+                                    @twitter
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="row content-listing-block">
+                        <div class="col-sm-4">
+                            <img src="../images/sample-images/img_medium.png" alt="Your Alt Title" title="Your Title" class="alignnone img-responsive">
+                        </div>
+                        <div class="col-sm-8">
+                            <h3>Level Three Heading</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                            <ul>
+                                <li>Unordered list item one.</li>
+                                <li>Unordered list item two.</li>
+                                <li>Unordered list item three.</li>
+                                <li>Unordered list item four.</li>
+                                <li>By the way, Wordpress does not let you create nested lists through the visual editor.</li>
+                            </ul>
+                            <!-- use .red class to .btn-primary element to make it red ex : <a class="btn-primary red" href="#"> -->
+                            <a class="btn-primary" href="#">
+                                inquire now
+                                <svg width="5" height="9" aria-hidden="true">
+                                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#rightarrow"></use>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <!-- /.content listing block -->
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div>wwwwwwwwwwww</div>
+        <h1>zzzz</h1>
+    </section>
+
+
+
+<?php include 'footer.php';?>
